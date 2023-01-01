79cd0d9931dd8e49e36ee312d44ebf8c0e279b6c
Yaron Aloni <jaron1990@gmail.com>
2023-01-01 14:44:42 +0200

    add parallelization in comment

--- run_balance.py
+++ run_balance.py
@@ -7,91 +7,86 @@ import balance
 import itertools
 import time
 import datetime
-import multiprocessing as mp
+from multiprocessing import Pool
 
 
-class octree_cell:
-    def __init__(self, point_start, size, beta_val, level):
-        assert(len(point_start)==3 and len(size)==3 and ((beta_val>=0 and beta_val<=1) or beta_val==-1))
-        self.point_start=point_start
-        self.size = size
-        self.point_end=point_start + size
-        self.beta_val=beta_val
-        self.level = level
-        combinations = list(itertools.product([0, 1], repeat=3))
-        self.corners = []
-        for comb in combinations:
-            corner = point_start + size*comb
-            self.corners.append(corner)
-
-    def split_cell(self):
-        new_cells = []
-        combinations = list(itertools.product([0, 1], repeat=3))
-        for comb in combinations:
-            start_step = comb*self.size/2
-            new_cell = octree_cell(self.point_start + start_step, self.size/2, self.beta_val, self.level+1)
-            new_cells.append(new_cell)
-        return new_cells
-
-##TODO - check if there's a different way... this MIGHT be buggy
-#returns 1 if inside, 0 if outside, -1 if partial
-def cell_inside_mesh(cell: octree_cell, shape: Trimesh):
-    inside = shape.contains(cell.corners + [(cell.point_start+cell.size/2)])
-    if (inside==True).all():    return 1
-    if (inside==False).all():   return 0
-    else:                       return -1
-
-class octree_graph:
-    def __init__(self, shape: Trimesh, max_level=7):
-        self.max_level=max_level
-        max_indices = np.array(np.max(shape.vertices, axis=0))
-        min_indices = np.array(np.min(shape.vertices, axis=0))
-        bbox = np.array([min_indices, max_indices])
-
-        octree_cells = []
-        octree_root_cell = octree_cell(point_start=min_indices, size=max_indices-min_indices, beta_val=-1, level=0)
-        self.remaining_cells_to_split = [octree_root_cell]
-        current_level=0
-       
+class octree_node:
+    def __init__(self, point_start, size, shape: Trimesh, current_level = 0, max_level=7):
         start = datetime.datetime.now()
-        level_start = datetime.datetime.now()
+        
+        self.shape=shape
+        self.current_level  = current_level
+        self.max_level      = max_level
+        self.deepest_level = current_level
 
-        while len(self.remaining_cells_to_split) > 0:
-            current_cell = self.remaining_cells_to_split.pop(0)
-            if current_cell.level>current_level:
-                end = datetime.datetime.now()
-                print(f"finished level {current_level}, {len(self.remaining_cells_to_split)} remained to split, {len(octree_cells)} already in octree")
-                current_level = current_cell.level
-                print(f"level took {(end-level_start).total_seconds()} seconds")
-                level_start = datetime.datetime.now()
+        self.point_start = point_start
+        self.size = size
+        self.point_end = self.point_start+self.size
 
+        self.combinations = list(itertools.product([0, 1], repeat=3))
+        self.corners = self.point_start + self.size*self.combinations
 
-            splitted = self.split_if_needed(current_cell, shape, octree_cells)
+        is_inside = self.is_inside_mesh(shape)
 
-            
+        if is_inside==1:
+            self.beta_val=1
+            self.children=None
+        elif is_inside==0:
+            self.beta_val=0
+            self.children=None
+        else:
+            self.beta_val=-1
+            if current_level>=max_level:
+                self.children=None
+            else:
+                self.children_starts = self.point_start + self.size/2*self.combinations
+
+                # # for parallel (not working because of some issue in trimesh):
+                # with Pool(8) as p:    
+                #     self.children = p.map(self.build_octree_node, range(8))
+                #     for child in self.children:
+                #         self.deepest_level = max(self.deepest_level, child.deepest_level)
+
+                # for series:
+                self.children = []
+                for i in range(8):
+                    self.children.append(octree_node(point_start=self.children_starts[i], size=self.size/2, shape=self.shape, current_level=self.current_level+1, max_level=self.max_level))
+                    self.deepest_level = max(self.deepest_level, self.children[-1].deepest_level)
+
         end = datetime.datetime.now()
 
-        print(f" \
-                finished level {current_level}, \
-                {len(self.remaining_cells_to_split)} remained to split, \
-                {len(octree_cells)} already in octree")
-        print(f"level took {(end-level_start).total_seconds()} seconds")
-
-        print(f"finished building octree. took {(end-start).total_seconds()} seconds")
+        if(self.current_level <=2):
+            print(f"finished building tree of level {self.current_level}. deepest level was {self.deepest_level}")
+            print(f"level took {(end-start).total_seconds()} seconds")
             
+    def build_octree_node(self, i):
+        child = octree_node(point_start=self.children_starts[i], size=self.size/2, shape=self.shape, current_level=self.current_level+1, max_level=self.max_level)
+        print(self.children_starts[i])
+        print(self.size/2)
+        print(self.current_level+1, self.max_level)
+        print(f"is child inside mesh? {child.is_inside_mesh(self.shape)}")
+        return child
+
     def split_if_needed(self, cell, shape, octree_cells_list):
         if(cell.level>=self.max_level):
             cell.beta_val=-1
             octree_cells_list.append(cell)
             return
 
-        cell_in_mesh = cell_inside_mesh(cell, shape)
+        cell_in_mesh = self.is_inside_mesh(shape)
         if cell_in_mesh==-1:
             self.remaining_cells_to_split += cell.split_cell()
         else:
             cell.beta_val=cell_in_mesh
             octree_cells_list.append(cell)
 
+    ##TODO - check if there's a different way... this MIGHT be buggy
+    #returns 1 if inside, 0 if outside, -1 if partial
+    def is_inside_mesh(self, shape: Trimesh):
+        inside = shape.contains(list(self.corners) + [(self.point_start+self.size/2)])
+        if (inside==True).all():    return 1
+        if (inside==False).all():   return 0
+        else:                       return -1
 
 def rujum_balance(src_dir, results_dir):
     """
@@ -107,7 +102,10 @@ def rujum_balance(src_dir, results_dir):
     # cell_inside_mesh(cell, shape)
     # select spin axis - point
   
-    graph = octree_graph(shape)
+    max_indices = np.array(np.max(shape.vertices, axis=0))
+    min_indices = np.array(np.min(shape.vertices, axis=0))
+    
+    graph = octree_node(point_start=min_indices, size=max_indices-min_indices, shape=shape, max_level=4)
 
     balance.balance(shape) #, spin_axis)
     
