834113e41381bbd31f0e8c78ca6214ba37ceebf1
idanshef <idan.sheffer@gmail.com>
2023-01-05 08:03:05 +0200

    made is_vertex_in_bbox one-line

--- utils.py
+++ utils.py
@@ -19,8 +19,5 @@ class Location(Enum):
     BOUNDARY = 3
 
 
-def is_vertex_in_bbox(vertices, point_start, point_end):
-    is_in_x = np.logical_and(vertices[:,0] > point_start[0], vertices[:,0] < point_end[0])
-    is_in_y = np.logical_and(vertices[:,1] > point_start[1], vertices[:,1] < point_end[1])
-    is_in_z = np.logical_and(vertices[:,2] > point_start[2], vertices[:,2] < point_end[2])
-    return np.logical_and(np.logical_and(is_in_x, is_in_y), is_in_z).any()
+def is_vertex_in_bbox(vertices: np.ndarray, cell_start: np.ndarray, cell_end: np.ndarray):
+    return np.logical_and(vertices > cell_start[None], vertices < cell_end[None]).all(axis=1).any()
