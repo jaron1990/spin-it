d07c26d498e84a00a351de424c8c5bcf39e2b614
idansheffer <idan.sheffer@mobileye.com>
2023-01-10 17:01:33 +0200

    fix tensors in optimizer

--- optimizer.py
+++ optimizer.py
@@ -1,4 +1,5 @@
-from octree import Octree
+import torch
+from octree import Octree, OctreeTensorHandler
 from scipy.optimize import minimize
 import numpy as np
 from scipy.sparse.csgraph import laplacian
@@ -16,35 +17,35 @@ class QPOptimizer:
         self.iter=0
         self.loss_list = []
 
-    def _calculate_s_total(self, internal_beta, octree):
-        s_internal = octree.get_internal_s_vector()
+    def _calculate_s_total(self, internal_beta, tree_tensor):
+        s_internal = OctreeTensorHandler.get_internal_s_vector(tree_tensor)
         s_internal_total = (s_internal.mul(internal_beta, axis=0)).sum()
 
-        s_boundary = octree.get_boundary_s_vector()
+        s_boundary = OctreeTensorHandler.get_boundary_s_vector(tree_tensor)
         s_boundary_total = s_boundary.sum()
 
         return s_internal_total + s_boundary_total
 
 
-    def loss(self, internal_beta, octree):
-        octree.set_s_vector()
+    def loss(self, internal_beta, tree_tensor):
+        tree_tensor = OctreeTensorHandler.set_s_vector(tree_tensor)
 
-        s_total = self._calculate_s_total(internal_beta, octree)
+        s_total = self._calculate_s_total(internal_beta, tree_tensor)
 
-        I = np.array([[s_total['s_yy']+s_total['s_zz'], -s_total['s_xy'], -s_total['s_xz']],
-                        [-s_total['s_xy'], s_total['s_xx']+s_total['s_zz'], -s_total['s_yz']],
-                        [-s_total['s_xz'], -s_total['s_yz'], s_total['s_xx']+s_total['s_yy']]])
-        I_CoM = I[:2,:2] - (s_total['s_z']**2/s_total['s_1']) * np.eye(2)
+        I = np.array([[s_total['s_yy'] + s_total['s_zz'], -s_total['s_xy'], -s_total['s_xz']],
+                      [-s_total['s_xy'], s_total['s_xx'] + s_total['s_zz'], -s_total['s_yz']],
+                      [-s_total['s_xz'], -s_total['s_yz'], s_total['s_xx'] + s_total['s_yy']]])
+        I_CoM = I[:2,:2] - (s_total['s_z']**2 / s_total['s_1']) * np.eye(2)
 
-        I_CoM_Rot = self._R*I_CoM*self._R.T
+        I_CoM_Rot = self._R * I_CoM * self._R.T
 
         I_a = I_CoM_Rot[0, 0]
         I_b = I_CoM_Rot[1, 1]
-        I_c = s_total['s_xx']+s_total['s_yy']
+        I_c = s_total['s_xx'] + s_total['s_yy']
 
 
-        f_yoyo = self._gamma_i*((I_a/I_c)**2+(I_b/I_c)**2)
-        f_top = self._gamma_c*(s_total['s_z']**2) + f_yoyo
+        f_yoyo = self._gamma_i * ((I_a / I_c)**2 + (I_b / I_c)**2)
+        f_top = self._gamma_c * (s_total['s_z']**2) + f_yoyo
 
         # print(f'iter={self.iter}')
         # self.iter+=1
@@ -74,26 +75,28 @@ class QPOptimizer:
         # return (f + self._gamma_l * 0.5 * beta.transpose() @ L @ beta)
 
 
-    def _constraint_s_x(self, beta, octree):
-        return self._calculate_s_total(beta, octree)['s_x']
-    def _constraint_s_y(self, beta, octree):
-        return self._calculate_s_total(beta, octree)['s_y']
-    def _constraint_s_xz(self, beta, octree):
-        return self._calculate_s_total(beta, octree)['s_xz']
-    def _constraint_s_yz(self, beta, octree):
-        return self._calculate_s_total(beta, octree)['s_yz']
+    def _constraint_s_x(self, beta, tree_tensor):
+        return self._calculate_s_total(beta, tree_tensor)['s_x']
     
-    def __call__(self, octree: Octree):
-        beta = octree.get_internal_beta()
-        octree.set_s_vector()
+    def _constraint_s_y(self, beta, tree_tensor):
+        return self._calculate_s_total(beta, tree_tensor)['s_y']
+    
+    def _constraint_s_xz(self, beta, tree_tensor):
+        return self._calculate_s_total(beta, tree_tensor)['s_xz']
+    
+    def _constraint_s_yz(self, beta, tree_tensor):
+        return self._calculate_s_total(beta, tree_tensor)['s_yz']
+    
+    def __call__(self, tree_tensor: torch.Tensor, roh: float):
+        beta = OctreeTensorHandler.get_internal_beta(tree_tensor)
+        tree_tensor = OctreeTensorHandler.set_s_vector(tree_tensor, roh)
 
-        bounds = [(0, 1) for i in range(len(beta))]
+        bounds = [(0, 1)] * len(beta)
+        problem = [{'type': 'eq', 'fun': self._constraint_s_x, 'args':(tree_tensor,)},           
+                    {'type': 'eq', 'fun': self._constraint_s_y, 'args':(tree_tensor,)},
+                    {'type': 'eq', 'fun': self._constraint_s_xz, 'args':(tree_tensor,)},
+                    {'type': 'eq', 'fun': self._constraint_s_yz, 'args':(tree_tensor,)}]
 
-        problem = [{'type': 'eq', 'fun': self._constraint_s_x, 'args':(octree,)},           
-                    {'type': 'eq', 'fun': self._constraint_s_y, 'args':(octree,)},
-                    {'type': 'eq', 'fun': self._constraint_s_xz, 'args':(octree,)},
-                    {'type': 'eq', 'fun': self._constraint_s_yz, 'args':(octree,)}]
-
-        result = minimize(self.loss, beta, bounds=bounds, constraints=problem, method='SLSQP', args=(octree,), options={'disp': True})
+        result = minimize(self.loss, beta, bounds=bounds, constraints=problem, method='SLSQP', args=(tree_tensor,), options={'disp': True})
         print(result)
-        octree.set_internal_beta(result.x)
+        tree_tensor = OctreeTensorHandler.set_internal_beta(tree_tensor, result.x)
